d679e0a06c77273e33bc5ce3d3a22bf50c229151
Hugo BARSOTTI <hugo.barsotti@necs.fr>
2023-01-12 19:51:54 +0100

    Ajout fenetre float pour diagnostics

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -78,6 +78,7 @@ local on_attach = function(_, bufnr)
     nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
 
     -- Lesser used LSP functionality
+    nmap('gl', vim.diagnostic.open_float, 'Open diagnostic float')
     nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
     nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
     nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
