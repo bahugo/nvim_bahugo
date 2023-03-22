003049274bdb84aca0fe0e25a8050ef3ba855b29
bahugo <barsottihugo@gmail.com>
2023-03-22 09:56:15 +0100

    Modification keymap pour éviter conflit

--- lua/bahugo_conf/remap.lua
+++ lua/bahugo_conf/remap.lua
@@ -36,5 +36,5 @@ vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>rs", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>cx", "<cmd>!chmod +x %<CR>", { silent = true, desc = "Change execution rights of current file" })
