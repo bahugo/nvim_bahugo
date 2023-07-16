adfd65c11b6e2658d917c43508519e542c163f26
Hugo BARSOTTI <hugo.barsotti@necs.fr>
2023-07-16 11:47:35 +0200

    Completed luasnip

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -296,7 +296,20 @@ require("fidget").setup {
 
 -- nvim-cmp setup
 local cmp = require 'cmp'
+
+-- Luasnip to handle code snippets
 local luasnip = require 'luasnip'
+vim.keymap.set({"i"}, "<C-K>", function() luasnip.expand() end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-L>", function() luasnip.jump( 1) end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-J>", function() luasnip.jump(-1) end, {silent = true})
+
+vim.keymap.set({"i", "s"}, "<C-E>", function()
+	if luasnip.choice_active() then
+		luasnip.change_choice(1)
+	end
+end, {silent = true})
+-- use vscode snippets
+require("luasnip.loaders.from_vscode").lazy_load()
 
 cmp.setup {
     snippet = {
