8b49edd4ac8c3b99f75d9cf6d4016b540a3b843a
bahugo <74469417+bahugo@users.noreply.github.com>
2023-11-13 12:24:42 +0100

    Added timeout to nvimtree git

--- lua/bahugo_conf/plugins/nvimtree.lua
+++ lua/bahugo_conf/plugins/nvimtree.lua
@@ -5,7 +5,14 @@ config=function()
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
 
-    require('nvim-tree').setup()
+    require('nvim-tree').setup(
+    {
+   git = {
+		enable = true,
+        timeout = 200,
+	},
+}
+)
 
     vim.keymap.set('n', '<c-n>', ':NvimTreeFindFileToggle<CR>')
 end
