7cceb68e3a21f257304b940032735a5c94d888b1
Hugo BARSOTTI <hugo.barsotti@necs.fr>
2023-12-20 22:39:48 +0100

    Added cycling files in harpoon

--- lua/bahugo_conf/plugins/harpoon.lua
+++ lua/bahugo_conf/plugins/harpoon.lua
@@ -10,9 +10,7 @@ return {
         vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end, { desc = "Harpoon - add file" } )
         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon - toggle" })
 
-        vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end, { desc = "Harpoon - go to file 1" })
-        vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end, { desc = "Harpoon - go to file 2" })
-        vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end, { desc = "Harpoon - go to file 3" })
-        vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end, { desc = "Harpoon - go to file 4" })
+        vim.keymap.set("n", "<C-h>", function() harpoon:list():next({ui_nav_wrap=true}) end, { desc = "Harpoon - cycle ->" })
+        vim.keymap.set("n", "<C-y>", function() harpoon:list():prev({ui_nav_wrap=true}) end, { desc = "Harpoon - cycle <-" })
     end
 }
