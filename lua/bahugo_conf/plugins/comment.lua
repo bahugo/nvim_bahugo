02d4dc5f3595b95cd4c28ab3598ddf513c825a79
Hugo BARSOTTI <hugo.barsotti@necs.fr>
2023-12-22 17:31:08 +0100

    Added comment for qmljs files

--- lua/bahugo_conf/plugins/comment.lua
+++ lua/bahugo_conf/plugins/comment.lua
@@ -4,6 +4,11 @@ return {
     -- Enable Comment.nvim
     config = function()
         require('Comment').setup()
+        local ft = require('Comment.ft')
+        -- 1. Using set function
+        ft
+        -- Set only line comment
+        .set('qmljs', '//%s')
     end,
     lazy = false
 }
