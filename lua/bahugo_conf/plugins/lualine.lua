8f1170f48636e42adcd904d5aea03c50a4ba9c79
bahugo <barsottihugo@gmail.com>
2024-01-09 18:20:40 +0100

    Added icons to lualine

--- lua/bahugo_conf/plugins/lualine.lua
+++ lua/bahugo_conf/plugins/lualine.lua
@@ -1,9 +1,10 @@
 return {
     'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
         require('lualine').setup {
             options = {
-                icons_enabled = false,
+                icons_enabled = true,
                 -- theme = 'codedark',
                 -- theme = 'rose-pine',
                 theme = "auto"
@@ -13,6 +14,13 @@ return {
                     {
                         'filename',
                         path = 1,
+                        diagnostics_color = {
+        -- Same values as the general color option can be used here.
+        error = 'DiagnosticError', -- Changes diagnostics' error color.
+        warn  = 'DiagnosticWarn',  -- Changes diagnostics' warn color.
+        info  = 'DiagnosticInfo',  -- Changes diagnostics' info color.
+        hint  = 'DiagnosticHint',  -- Changes diagnostics' hint color.
+      },
                     }
                 }
             }
