f1d83177d17b608e736d0c830ea6d0b3f2cf3459
bahugo <barsottihugo@gmail.com>
2024-02-07 15:48:28 +0100

    Added cmdline completion

--- lua/bahugo_conf/plugins/lsp.lua
+++ lua/bahugo_conf/plugins/lsp.lua
@@ -5,20 +5,10 @@ return {
         -- LSP Support
         { 'williamboman/mason.nvim' },
         { 'williamboman/mason-lspconfig.nvim' },
-
-        -- Autocompletion
-        { 'hrsh7th/nvim-cmp' },
-        { 'hrsh7th/cmp-buffer' },
-        { 'hrsh7th/cmp-path' },
-        { 'saadparwaiz1/cmp_luasnip' },
+        -- LSP completion
         { 'hrsh7th/cmp-nvim-lsp' },
-        { 'hrsh7th/cmp-nvim-lua' },
-
-        -- Snippets
-        { 'L3MON4D3/LuaSnip' },
-        { 'rafamadriz/friendly-snippets' },
         -- Useful status Update for LSP
-        { 'j-hui/fidget.nvim',                tag = "legacy" },
+        { 'j-hui/fidget.nvim', },
         -- Additional lua configuration for nvim
         { 'folke/neodev.nvim' },
         "simrat39/rust-tools.nvim",
@@ -328,72 +318,6 @@ return {
                 blend = 0,
             },
             -- ... the rest of your fidget config
-        }
-
-        -- nvim-cmp setup
-        local cmp = require 'cmp'
-
-        -- Luasnip to handle code snippets
-        local luasnip = require 'luasnip'
-        vim.keymap.set({ "i" }, "<C-K>", function() luasnip.expand() end, { silent = true })
-        vim.keymap.set({ "i", "s" }, "<C-L>", function() luasnip.jump(1) end, { silent = true })
-        vim.keymap.set({ "i", "s" }, "<C-J>", function() luasnip.jump(-1) end, { silent = true })
-
-        vim.keymap.set({ "i", "s" }, "<C-E>", function()
-            if luasnip.choice_active() then
-                luasnip.change_choice(1)
-            end
-        end, { silent = true })
-        luasnip.add_snippets("python", {
-            luasnip.snippet("debug", {
-                luasnip.text_node({ "import debugpy", "debug_port = 5678",
-                    "debugpy.configure(qt=\"auto\", )", "debugpy.listen(debug_port)",
-                    "debugpy.wait_for_client()"}),
-            }),
-        }, {
-            key = "debug",
-        })
-
-        -- use vscode snippets
-        require("luasnip.loaders.from_vscode").lazy_load()
-
-        cmp.setup {
-            snippet = {
-                expand = function(args)
-                    luasnip.lsp_expand(args.body)
-                end,
-            },
-            mapping = cmp.mapping.preset.insert {
-                ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-                ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                ['<C-Space>'] = cmp.mapping.complete(),
-                ['<CR>'] = cmp.mapping.confirm {
-                    behavior = cmp.ConfirmBehavior.Replace,
-                    select = true,
-                },
-                ['<Tab>'] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_next_item()
-                    elseif luasnip.expand_or_jumpable() then
-                        luasnip.expand_or_jump()
-                    else
-                        fallback()
-                    end
-                end, { 'i', 's' }),
-                ['<S-Tab>'] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_prev_item()
-                    elseif luasnip.jumpable(-1) then
-                        luasnip.jump(-1)
-                    else
-                        fallback()
-                    end
-                end, { 'i', 's' }),
-            },
-            sources = {
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' },
-            },
         }
 
         require('nvim-lightbulb').setup({ autocmd = { enabled = true } })
