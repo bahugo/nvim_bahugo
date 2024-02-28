aa44931680b8d82a35e3fe6ccc9d9323520a63a2
bahugo <barsottihugo@gmail.com>
2024-02-28 16:00:54 +0100

    Updated telescope config (path_display) + search diag for current buffer only

--- lua/bahugo_conf/plugins/telescope.lua
+++ lua/bahugo_conf/plugins/telescope.lua
@@ -1,3 +1,5 @@
+local telescope = require('telescope')
+local telescope_builtin = require('telescope.builtin')
 return {
 
     'nvim-telescope/telescope.nvim',
@@ -5,19 +7,20 @@ return {
     dependencies = { { 'nvim-lua/plenary.nvim' } },
     keys={
 
-        {mode= 'n', '<C-p>',      function() require('telescope.builtin').git_files() end,  desc = "Telescope git_files" },
-        {mode= 'n', '<leader>sf', function() require('telescope.builtin').find_files() end,  desc = '[S]earch [F]iles' },
-        {mode= 'n', '<leader>sh', function() require('telescope.builtin').help_tags() end,  desc = '[S]earch [H]elp' },
-        {mode= 'n', '<leader>sw', function() require('telescope.builtin').grep_string() end,  desc = '[S]earch current [W]ord' },
-        {mode= 'n', '<leader>sg', function() require('telescope.builtin').live_grep() end,  desc = '[S]earch by [G]rep' },
-        {mode= 'n', '<leader>sd', function() require('telescope.builtin').diagnostics() end,  desc = '[S]earch [D]iagnostics' },
-        {mode= 'n', '<leader>sc', function() require('telescope').extensions.neoclip.default() end,  desc = '[S]earch [C]lipboard' },
-        {mode= 'n', '<leader>dc', function() require('telescope').extensions.dap.commands() end,  desc = '[D]ap [C]ommands' },
-        {mode= 'n', '<leader>dg', function() require('telescope').extensions.dap.configurations() end,  desc = '[D]ap confi[G]urations' },
+        {mode= 'n', '<C-p>',      function() telescope_builtin.git_files() end,  desc = "Telescope git_files" },
+        {mode= 'n', '<leader>sf', function() telescope_builtin.find_files() end,  desc = '[S]earch [F]iles' },
+        {mode= 'n', '<leader>sh', function() telescope_builtin.help_tags() end,  desc = '[S]earch [H]elp' },
+        {mode= 'n', '<leader>sw', function() telescope_builtin.grep_string() end,  desc = '[S]earch current [W]ord' },
+        {mode= 'n', '<leader>sg', function() telescope_builtin.live_grep() end,  desc = '[S]earch by [G]rep' },
+        {mode= 'n', '<leader>sd', function() telescope_builtin.diagnostics({bufnr=0}) end,  desc = '[S]earch [D]iagnostics in current buffer' },
+        {mode= 'n', '<leader>sc', function() telescope.extensions.neoclip.default() end,  desc = '[S]earch [C]lipboard' },
+        {mode= 'n', '<leader>dc', function() telescope.extensions.dap.commands() end,  desc = '[D]ap [C]ommands' },
+        {mode= 'n', '<leader>dg', function() telescope.extensions.dap.configurations() end,  desc = '[D]ap confi[G]urations' },
     },
     config = function()
         -- This is your opts table
-        require("telescope").setup {
+        telescope.setup {
+            path_display="smart",
             extensions = {
                 -- utilisation de ui select pour afficher les code actions dans telescope
                 ["ui-select"] = {
@@ -30,7 +33,7 @@ return {
         }
         -- To get ui-select loaded and working with telescope, you need to call
         -- load_extension, somewhere after setup function:
-        require("telescope").load_extension("ui-select")
-        require('telescope').load_extension('dap')
+        telescope.load_extension("ui-select")
+        telescope.load_extension('dap')
     end
 }
