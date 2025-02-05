99270f1f9897f77daf94b601c81343151a6a4a32
bahugo <barsottihugo@gmail.com>
2025-02-05 14:43:53 +0100

    Removed toggleterm + changed harpoon and quickfix list mappings

--- lua/bahugo_conf/plugins/harpoon.lua
+++ lua/bahugo_conf/plugins/harpoon.lua
@@ -8,9 +8,9 @@ return {
         -- REQUIRED
 
         vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = "Harpoon - add file" } )
-        vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon - toggle" })
+        vim.keymap.set("n", "<A-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon - toggle" })
 
-        vim.keymap.set("n", "<C-S-n>", function() harpoon:list():next({ui_nav_wrap=true}) end, { desc = "Harpoon - cycle ->" })
-        vim.keymap.set("n", "<C-S-p>", function() harpoon:list():prev({ui_nav_wrap=true}) end, { desc = "Harpoon - cycle <-" })
+        vim.keymap.set("n", "<A-n>", function() harpoon:list():next({ui_nav_wrap=true}) end, { desc = "Harpoon - cycle ->" })
+        vim.keymap.set("n", "<A-p>", function() harpoon:list():prev({ui_nav_wrap=true}) end, { desc = "Harpoon - cycle <-" })
     end
 }
--- lua/bahugo_conf/remap.lua
+++ lua/bahugo_conf/remap.lua
@@ -6,6 +6,7 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", {desc= "Move Up"})
 
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
+
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
@@ -28,10 +29,10 @@ vim.keymap.set("n", "<A-l>", "<C-w>l", {desc="Window ->"} )
 vim.keymap.set("n", "<A-j>", "<C-w>j", {desc="Window DOWN"} )
 vim.keymap.set("n", "<A-k>", "<C-w>k", {desc="Window UP"} )
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+vim.keymap.set("n", "<A-S-p>", "<cmd>cprev<CR>zz", {desc="Quickfix <-"})
+vim.keymap.set("n", "<A-S-n>", "<cmd>cnext<CR>zz", {desc="Quickfix ->"})
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz", {desc="List ->"})
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz", {desc="List <-"})
 
 vim.keymap.set("n", "<leader>rs", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>cx", "<cmd>!chmod +x %<CR>", { silent = true, desc = "Change execution rights of current file" })
