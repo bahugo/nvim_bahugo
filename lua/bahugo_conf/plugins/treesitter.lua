325d2accf8784757f8e11b655c100d4266654d45
hugo barsotti <hugo.barsotti@edf.fr>
2026-04-14 15:41:12 +0200

    Removing comment and treesitter (using nvim 0.12 native capabilities)

--- lua/bahugo_conf/plugins/treesitter.lua
+++ lua/bahugo_conf/plugins/treesitter.lua
@@ -1,32 +1,41 @@
-return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        dependencies = {
-            'nvim-treesitter/nvim-treesitter-textobjects',
-            -- 'nvim-treesitter/playground',
-        },
-        build = ":TSUpdate",
-        event = { "BufReadPre", "BufNewFile" },
-        config = function()
-            local config = require("nvim-treesitter.configs")
-            config.setup({
-                ensure_installed = {"awk", "qmldir", "qmljs", "cpp", "c_sharp", "python", "c", "astro",
-                    "html", "javascript", "typescript", "jsdoc", "lua", "luadoc", "rust", "sql",
-                    "yaml", "csv", "xml", "bash", "fortran", "cmake", "json", "rst", "toml",
-                    "markdown", "markdown_inline", "comment", "ssh_config",
-                    "git_config", "git_rebase", "gitcommit", "gitattributes", "gitignore",
-                    "regex", "fish", "doxygen", "dockerfile", "diff",
-                    "mermaid", "meson", "tmux",
-                    -- latex requires tree-sitter cli to be installed
-                    -- "latex",
-                },
-                highlight = { enable = true },
-                indent = { enable = true },
-            })
-
-            vim.treesitter.language.register("qmljs", "qml")
-            vim.treesitter.language.register("python", "aster")
-        end
-    },
-
-}
+-- return {
+--     {
+--         "nvim-treesitter/nvim-treesitter",
+--         dependencies = {
+--             'nvim-treesitter/nvim-treesitter-textobjects',
+--             -- 'nvim-treesitter/playground',
+--         },
+--         build = ":TSUpdate",
+--         event = { "BufReadPre", "BufNewFile" },
+--         config = function()
+--             local config = require("nvim-treesitter.configs")
+--             config.setup({
+--                 ensure_installed = {"awk", "qmldir", "qmljs", "cpp", "c_sharp", "python", "c", "astro",
+--                     "html", "javascript", "typescript", "jsdoc", "lua", "luadoc", "rust", "sql",
+--                     "yaml", "csv", "xml", "bash", "fortran", "cmake", "json", "rst", "toml",
+--                     "markdown", "markdown_inline", "comment", "ssh_config",
+--                     "git_config", "git_rebase", "gitcommit", "gitattributes", "gitignore",
+--                     "regex", "fish", "doxygen", "dockerfile", "diff",
+--                     "mermaid", "meson", "tmux",
+--                     -- latex requires tree-sitter cli to be installed
+--                     -- "latex",
+--                 },
+--                 highlight = { enable = true },
+--                 indent = { enable = true },
+--             })
+--
+--             vim.treesitter.language.register("qmljs", "qml")
+--             vim.treesitter.language.register("python", "aster")
+--         end
+--     },
+--
+-- }
+-- return {{
+--   'nvim-treesitter/nvim-treesitter',
+--   lazy = false,
+--   build = ':TSUpdate'
+-- }
+-- }
+vim.treesitter.language.register("qmljs", "qml")
+vim.treesitter.language.register("python", "aster")
+return {}
